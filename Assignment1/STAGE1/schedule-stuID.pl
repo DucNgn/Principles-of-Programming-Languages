8cf314637053c33493e9a34ef95705fa05ef600c
Antonio Verdicchio <antonioverdicchio@gmail.com>
2020-05-06 11:18:46 -0700

    Edited stuID.pl to contain my courses

--- Assignment1/STAGE1/schedule-stuID.pl
+++ Assignment1/STAGE1/schedule-stuID.pl
@@ -1,8 +1,12 @@
 % Stage 1
-team(['40064649']).
+team(['40064649', '40062102']).
 student_info('40064649', 'Duc', 'Nguyen').
-take_course('40064649', 'comp', '348', 'aa').
-take_course('40064649', 'encs', '282', 'ab').
+student_info('40062102', 'Antonio', 'Verdicchio').
+takes_course('40064649', 'comp', '348', 'aa').
+takes_course('40064649', 'encs', '282', 'ab').
+takes_course('40062101', 'comp', '348', 'aa').
+takes_course('40062102', 'econ', '201', 'ec').
+takes_course('40062102', 'econ', '203', 'ec').
 course_schedule('comp', '348', 'aa', 'mon', '1445', '1715').
 course_schedule('comp', '348', 'aa', 'wed', '1445', '1715').
 course_schedule('comp', '348', 'aaae', 'wed', '1315', '1405').
@@ -10,5 +14,9 @@ course_schedule('encs', '282', 'ab', 'tue', '1015', '1245').
 course_schedule('encs', '282', 'ab', 'thu', '1015', '1245').
 course_schedule('encs', '282', 'abal', 'tue', '1445', '1625').
 course_schedule('encs', '282', 'abal', 'thu', '1445', '1625').
-
-
+course_schedule('comp', '348', 'aa', 'mon', '1445', '1715').
+course_schedule('comp', '348', 'aa', 'wed', '1445', '1715').
+course_schedule('comp', '348', 'aaaf', 'mon', '1315', '1405').
+course_schedule('comp', '348', 'aaaf', 'wed', '1315', '1405').
+course_schedule('econ', '201', 'ec', 'tue', '1755', '2015').
+course_schedule('econ', '203', 'ec', 'mon', '1755', '2015').
