785ba646343f18bfb06ce3b33cb2321f716c80af
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-05-17 20:43:50 -0400

    Update Q11-Lucas Sequence.pl

--- Assignment1/STAGE2/Q11-Lucas Sequence.pl
+++ Assignment1/STAGE2/Q11-Lucas Sequence.pl
@@ -4,3 +4,6 @@ lucas(2, [1,2]).
 
 % recursion
 lucas(N, [L1,L2,L3|T]) :- N > 2, N1 is N - 1, lucas(N1, [L2,L3|T]), L1 is L2 + L3.
+
+% we can get the n first number using the following query (for example for n = 7):
+% ?- lucas(7,X).
