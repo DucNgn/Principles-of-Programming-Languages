3218e8d37f0589d39546a77379443d168f1ec56a
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-05-31 19:34:32 -0400

    Update question-6.lisp

--- Assignment2/question-6.lisp
+++ Assignment2/question-6.lisp
@@ -1,6 +1,6 @@
 (defun triangle (num) 
 (cond 
-((or (not (integerp num)) (zerop num)) (print "input invalid; please enter a positive or a negative integer"))
+((or (not (integerp num)) (zerop num)) (write-line "input invalid; please enter a positive or a negative integer"))
 ((< num 0)
 (dotimes (n (abs num)) 
 (dotimes (p n)
@@ -21,4 +21,13 @@
 )
 )
  )
+ 
 
+ (write-line "(triangle 7) will output:")
+ (triangle 7)
+ (write-line "(triangle 4) will output:")
+ (triangle 4)
+  (write-line "(triangle -5) will output:")
+ (triangle -5)
+  (write-line "(triangle 2.5) will output:")
+ (triangle 2.5)
