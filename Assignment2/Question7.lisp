3286ceb100c7de1917deee2823c5fac89060cbf1
Antonio Verdicchio <antonioverdicchio@gmail.com>
2020-05-31 14:50:18 -0700

    Added solution for Question 7

---
+++ Assignment2/Question7.lisp
@@ -0,0 +1,25 @@
+
+(defun collatz (input)
+    (setq n (car input))
+    (setq output '())
+    (if (plusp n) 
+        (generatelist n output)
+        (print (Please enter a valid number))
+     )
+    (print (reverse output))
+    )
+
+(defun generatelist (num modifiedlist)
+    (if (not (= 1 num))
+        (progn
+            (if (evenp num)
+                 (setq x (/ num 2))
+                 (setq x (+ 1 (* 3 num))))
+             (setf output (cons x modifiedlist))
+             (generatelist x output))
+     )
+ )
+
+(setf twentynumbers '((1) (2) (3) (4) (5) (6) (7) (8) (9) (10) (11) (12) (13) (14) (15) (16) (17) (18) (19) (20)))
+(loop for x in twentynumbers
+      do (collatz x))
