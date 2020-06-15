00a93f30e666be1d17a4970f2f3a40ea6298a9c0
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-06-14 21:57:39 -0400

    Q5 solution

---
+++ Assignment3/Question5.rb
@@ -0,0 +1,43 @@
+a = []
+File.open("Information.txt").each {|line| a.push(line)}
+
+for element in a
+  info = element.split
+
+  if info[0] == "rectangle" 
+    if info.length == 3 and info[1].to_i >=0 and info[2].to_i >=0
+      r = Rectangle.new(info[1].to_i,info[2].to_i)
+      r.print
+    else 
+      puts "Invalid Rectangle"
+    end
+  
+
+  elsif info[0] == "circle" 
+    if info.length == 2 and info[1].to_i >=0
+      c = Circle.new(info[1].to_i)
+      c.print()
+    else 
+      puts "Invalid Circle"
+    end
+  
+
+  elsif info[0] == "ellipse" 
+    if info.length == 3 and info[1].to_i >=0 and info[2].to_i >=0 and info[2].to_i >= info[1].to_i
+      e = Ellipse.new(info[2].to_i,info[1].to_i)
+      puts "Ellipse, perimeter: undefined" + ", area: "+e.area().to_s
+    else 
+      puts "Invalid Ellipse"
+    end
+  
+
+  elsif info[0] == "shape" 
+    if info.length == 1
+      s = Shape.new()
+      puts "Shape, perimeter: undefined, area: undefined"
+    else 
+      puts "Invalid Shape"
+    end
+  end
+
+end
